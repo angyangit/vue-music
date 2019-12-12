629cfbbf4916b5e3f6fec7870c2535aaa80d918d
angyang <angyangit@163.com>
2019-12-12 15:57:43 +0800

    axios

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,4 +1,4 @@
-const Loading = require('../components/Loading.vue')
+// const Loading = require('../components/Loading.vue')
 const axiosReq = require('axios')
 const urlMap = {
   development: '/',
@@ -9,34 +9,34 @@ const baseUrl = urlMap[process.env.NODE_ENV]
 const ERR_OK = 0
 var helpers = {
   get(parm, flag) {
-    if (!flag) this.show()
+    // if (!flag) this.show()
     return axiosReq.get(baseUrl + parm.url, {
       params: parm.data
     }).then((res) => {
-      if (!flag) this.hide()
+      // if (!flag) this.hide()
       console.dir(res)
       if (res.data.code === ERR_OK) {
         return Promise.resolve(res.data)
       }
     }).catch((e) => {
-      if (!flag) this.hide()
+      // if (!flag) this.hide()
     })
-  },
-  show(text) {
-    this.LoadingCom = this.LoadingCom || Loading.$create({
-      $props: {
-        text: text
-      }
-    })
-    this.LoadingCom.show()
-  },
-  hide(text) {
-    this.LoadingCom = this.LoadingCom || Loading.$create({
-      $props: {
-        text: text
-      }
-    })
-    this.LoadingCom.hide()
   }
+  // show(text) {
+  //   this.LoadingCom = this.LoadingCom || Loading.$create({
+  //     $props: {
+  //       text: text
+  //     }
+  //   })
+  //   this.LoadingCom.show()
+  // },
+  // hide(text) {
+  //   this.LoadingCom = this.LoadingCom || Loading.$create({
+  //     $props: {
+  //       text: text
+  //     }
+  //   })
+  //   this.LoadingCom.hide()
+  // }
 }
 module.exports = helpers
