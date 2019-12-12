c15b819bf244e06f24122f17a2144a64891d4fac
angyang <angyangit@163.com>
2019-12-12 18:00:42 +0800

    提交

---
+++ prod.server.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var helper = require('./src/api/helpers')
+var port = 3000
+
+var app = express()
+
+var apiRoutes = express.Router()
+
+apiRoutes.get('/getRecommmend', function (req, res) {
+  const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?'
+  helper.get({url, params: req.query}).then((response) => {
+    res.json(response.data)
+    console.log('getRecommmend', response)
+  }).catch((e) => {
+    console.log('getRecommmend', e)
+  })
+})
+apiRoutes.get('/getRanking', function (req, res) {
+    const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?'
+    helper.get({url, params: req.query}).then((response) => {
+      res.json(response.data)
+      console.log('getRanking', response)
+    }).catch((e) => {
+      console.log('getRanking', e)
+    })
+  }
+)
+
+app.use('/api', apiRoutes)
+
+app.use(express.static('./dist'))
+
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + port + '\n')
+})
